84c8e92d9eada099ab5bd68de5c38c0915582411
Padma Gorrela <padma_gorrela@epam.com>
2024-04-19 14:29:55 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!121212");
+String s1="s1";
+String s0="s0";
+String s2="s2";
+String s3="s1";
+String s4="s2";
+Console.WriteLine("this is {0}",s0);
+Console.WriteLine("this is",s0);
+
+
